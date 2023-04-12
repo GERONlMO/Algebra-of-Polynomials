8ba218cf21ef4a577236a9bfb2b0154d1dea718f
FirewallGR <qwert.ting@bk.ru>
2023-04-13 01:52:28 +0300

    moved template realization to header files

--- Algebra-of-Polynomials/libs/Functional/lib_SyntaxTree/SyntaxTree.h
+++ Algebra-of-Polynomials/libs/Functional/lib_SyntaxTree/SyntaxTree.h
@@ -2,12 +2,9 @@
 #define LIB_SYNTAXTREE_SYNTAXTREE_H_
 
 #include <map>
-#include "..\lib_Node\Node.h"
-#include "..\lib_Stack\Stack.h"
-#include "..\lib_Monom\Monom.h"
-#include <stdio.h>
-
-
+#include "Node.h"
+#include "Stack.h"
+#include "Monom.h"
 
 template <typename T, typename T1>
 class SyntaxTree {
@@ -23,19 +20,97 @@ class SyntaxTree {
     Stack<T> stack = Stack<T>(10);
 	Node<T>* root = nullptr;
     Node<T>* treeRoot = nullptr;
-	Node<T>* addNodeRecursive(Node<T>* current, T data);
-    void getTreeRoot();
-	void createSyntaxTreeStack(Node<T>* current);
-	void deleteTree(Node<T>* current);
+	Node<T>* addNodeRecursive(Node<T>* current, std::string data) {
+        bool flag = false;
+
+        if (current == nullptr){
+            Node<T>* tmp(data);
+            return tmp;
+        }
+
+        else if (current->getRight() == nullptr) {
+            if (data == "-" || data == "/" || data == "^")
+                flag = true;
+            current->setRight(addNodeRecursive(current->getRight(), data));
+        }
+        else if (current->getLeft() == nullptr) {
+            if (data == "-" || data == "/" || data == "^")
+                flag = true;
+            current->setLeft(addNodeRecursive(current->getLeft(), data));
+        }
+        if (!operationsPriority.contains(current->getRight()->getValue()) && !operationsPriority.contains(current->getLeft()->getValue())) {
+            current = current->getTop();
+            return current;
+        }
+
+        if (flag) {
+            current = current->getLeft();
+            current->setTop(root);
+            current->setLeft(addNodeRecursive(current->getLeft(), data));
+        } else {
+            current = current->getRight();
+            current->setTop(root);
+            current->setRight(addNodeRecursive(current->getRight(), data));
+        }
+
+        return current;
+    };
+    void getTreeRoot() {
+        while (root->getTop() != root) root = root->getTop();
+        treeRoot = root;
+    };
+	void createSyntaxTreeStack(Node<T> *current) {
+        if (current == nullptr) throw std::logic_error("The tree is empty");
+        if (current->getRight() != nullptr)
+            stack.push(current->getRight()->getValue());
+        if (current->getLeft() != nullptr)
+            stack.push(current->getLeft()->getValue());
+        if (operationsPriority.contains(current->getRight()->getValue())) {
+            current = current->getRight();
+            evaluateRecursive(current);
+        }
+        if (operationsPriority.contains(current->getLeft()->getValue())) {
+            current = current->getLeft();
+            evaluateRecursive(current);
+        }
+    };
+	void deleteTree(Node<T>* current) {
+        if (current != nullptr) {
+            deleteTree(current->getLeft());
+            deleteTree(current->getRight());
+            delete current;
+        }
+    };
 public:
-	SyntaxTree();
-	~SyntaxTree();
+    SyntaxTree() {
+        root = nullptr;
+    };
+    ~SyntaxTree() {
+        deleteTree(root);
+    };
 
-	void create(T* source, int size);
-	void insert(T obj);
+	void create(T* source, int size) {
+        for (int i = size - 1; i > 0; i--) {
+            insert(source[i]);
+        }
+    };
+	void insert(T obj) {
+        root = addNodeRecursive(root, obj);
+        getTreeRoot();
+    };
+	int evaluate() {
+        Node<T>* temp = treeRoot;
+        createSyntaxTreeStack(temp);
+        Stack<T> additional = Stack<T>(10);
+        while (!stack.isEmpty()) {
 
-	int evaluate();
-
-    std::string toString();
+        }
+    };
+    std::string toString() {
+        std::string result = "layout 1 - " + treeRoot->getValue() + "\n" +
+                "layout 2 - " + treeRoot->getLeft()->getValue() + " " + treeRoot->getRight()->getValue() +
+                "\nlayout 3 - " + treeRoot->getLeft()->getLeft()->getValue() + " " + treeRoot->getLeft()->getRight()->getValue() + " " + treeRoot->getRight()->getLeft()->getValue() + " " + treeRoot->getRight()->getRight()->getValue();
+        return result;
+    };
 };
 #endif // !LIB_SYNTAXTREE_SYNTAXTREE_H_
